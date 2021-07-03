bce5907bb606041e86c0b5926701022792bb5ca1
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2021-07-04 00:24:05 +0700

    Update edu.sh

--- edu.sh
+++ edu.sh
@@ -3,11 +3,11 @@
 # ==============================
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/edu-ssll https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/edu-ssll.py
-chmod +x /usr/local/bin/edu-ssll
+wget -q -O /usr/local/bin/edu-ssl https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/cdn-ssl.py
+chmod +x /usr/local/bin/edu-ssl
 
 # Installing Service
-cat > /etc/systemd/system/edu-ssll.service << END
+cat > /etc/systemd/system/edu-ssl.service << END
 [Unit]
 Description=Python Edu ssl By bokirtampan
 Documentation=https://bokirtampan.xyz
@@ -19,7 +19,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-ssll 2053
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-ssl 2053
 Restart=on-failure
 
 [Install]
@@ -27,7 +27,7 @@ WantedBy=multi-user.target
 END
 
 systemctl daemon-reload
-systemctl enable edu-ssll
-systemctl restart edu-ssll
+systemctl enable edu-ssl
+systemctl restart edu-ssl
 
 clear
