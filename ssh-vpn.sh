e9a4c55c2e939c6191a675f08d389a3159bbd7bb
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2021-07-07 19:55:02 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -153,6 +153,7 @@ apt-get remove --purge exim4 -y
 
 # install wget and curl
 apt -y install wget curl
+apt -y install python
 
 # set time GMT +7
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
