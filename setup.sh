c1f45baafdc7aa70c97566c8c60006cbb76e7f6b
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2021-07-28 13:05:30 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,6 +39,8 @@ wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/sodosok.sh
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ins-trojango.sh && chmod +x ins-trojango.sh && screen -S Trojan-go ./ins-trojango.sh
+
 #install L2TP
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
 wget https://raw.githubusercontent.com/bokir-tampan/ranjau-darat/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
