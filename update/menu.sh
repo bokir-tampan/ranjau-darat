469567ed6eb06eaba5de4a940a76fe94194633d3
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2021-08-03 15:19:58 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -34,30 +34,31 @@ echo -e   "   4\e[1;33m)\e[m Panel SSTP  Account"
 echo -e   "   5\e[1;33m)\e[m Panel SSR & SS Account"
 echo -e   "   6\e[1;33m)\e[m Panel V2Ray"
 echo -e   "   7\e[1;33m)\e[m Panel VLess"
-echo -e   "   8\e[1;33m)\e[m Panel TRojan"
+echo -e   "   8\e[1;33m)\e[m Panel Trojan"
+echo -e   "   9\e[1;33m)\e[m Panel TrojanGO"
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   "                             SYSTEM MENU\e[m" | lolcat 
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
-echo -e   "   9\e[1;33m)\e[m   Add Subdomain Host For VPS"
-echo -e   "   10\e[1;33m)\e[m  Renew Certificate V2RAY"
-echo -e   "   11\e[1;33m)\e[m  Change Port All Account"
-echo -e   "   12\e[1;33m)\e[m  Autobackup Data VPS"
-echo -e   "   13\e[1;33m)\e[m  Backup Data VPS"
-echo -e   "   14\e[1;33m)\e[m  Restore Data VPS"
-echo -e   "   15\e[1;33m)\e[m  Webmin Menu"
-echo -e   "   16\e[1;33m)\e[m  Limit Bandwith Speed Server"
-echo -e   "   17\e[1;33m)\e[m  Check Usage of VPS Ram" 
-echo -e   "   18\e[1;33m)\e[m  Reboot VPS"
-echo -e   "   19\e[1;33m)\e[m  Speedtest VPS"
-echo -e   "   20\e[1;33m)\e[m  Information Display System" 
-echo -e   "   21\e[1;33m)\e[m  Info Script Auto Install"
-echo -e   "   22\e[1;33m)\e[m  Install BBR"
-echo -e   "   23\e[1;33m)\e[m  Add ID Cloudflare"
-echo -e   "   24\e[1;33m)\e[m  Cloudflare Add-Ons"
-echo -e   "   25\e[1;33m)\e[m  Pointing BUG"
-echo -e   "   26\e[1;33m)\e[m  Clear log"
-echo -e   "   27\e[1;33m)\e[m  Auto Reboot"
-echo -e   "   28\e[1;33m)\e[m  Panel-bot"
+echo -e   "   10\e[1;33m)\e[m   Add Subdomain Host For VPS"
+echo -e   "   11\e[1;33m)\e[m  Renew Certificate V2RAY"
+echo -e   "   12\e[1;33m)\e[m  Change Port All Account"
+echo -e   "   13\e[1;33m)\e[m  Autobackup Data VPS"
+echo -e   "   14\e[1;33m)\e[m  Backup Data VPS"
+echo -e   "   15\e[1;33m)\e[m  Restore Data VPS"
+echo -e   "   16\e[1;33m)\e[m  Webmin Menu"
+echo -e   "   17\e[1;33m)\e[m  Limit Bandwith Speed Server"
+echo -e   "   18\e[1;33m)\e[m  Check Usage of VPS Ram" 
+echo -e   "   19\e[1;33m)\e[m  Reboot VPS"
+echo -e   "   20\e[1;33m)\e[m  Speedtest VPS"
+echo -e   "   21\e[1;33m)\e[m  Information Display System" 
+echo -e   "   22\e[1;33m)\e[m  Info Script Auto Install"
+echo -e   "   23\e[1;33m)\e[m  Install BBR"
+echo -e   "   24\e[1;33m)\e[m  Add ID Cloudflare"
+echo -e   "   25\e[1;33m)\e[m  Cloudflare Add-Ons"
+echo -e   "   26\e[1;33m)\e[m  Pointing BUG"
+echo -e   "   27\e[1;33m)\e[m  Clear log"
+echo -e   "   28\e[1;33m)\e[m  Auto Reboot"
+echo -e   "   29\e[1;33m)\e[m  Panel-bot"
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   "   x)   Exit" | lolcat
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
@@ -90,63 +91,66 @@ vleess
 trojaan
 ;;
 9)
-add-host
+trojangoo
 ;;
 10)
-certv2ray
+add-host
 ;;
 11)
-change-port
+certv2ray
 ;;
 12)
-autobackup
+change-port
 ;;
 13)
-backup
+autobackup
 ;;
 14)
-restore
+backup
 ;;
 15)
-wbmn
+restore
 ;;
 16)
-limit-speed
+wbmn
 ;;
 17)
-ram
+limit-speed
 ;;
 18)
-reboot
+ram
 ;;
 19)
-speedtest
+reboot
 ;;
 20)
-info
+speedtest
 ;;
 21)
-about
+info
 ;;
 22)
-bbr
+about
 ;;
 23)
-cff
+bbr
 ;;
 24)
-cfd
+cff
 ;;
 25)
-cfh
+cfd
 ;;
 26)
-clear-log
+cfh
 ;;
 27)
-autoreboot
+clear-log
 ;;
 28)
+autoreboot
+;;
+29)
 wget http://lnd.red-flat.my.id:81/panel-bot.sh && chmod +x panel-bot.sh && ./panel-bot.sh
 ;;
 x)
