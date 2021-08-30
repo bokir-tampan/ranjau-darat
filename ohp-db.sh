dc5a8d33018792ab8b7bb8e5ce7fab5a124ffa22
bokir-tampan <86706494+bokir-tampan@users.noreply.github.com>
2021-08-30 16:55:48 +0700

    Update ohp-db.sh

--- ohp-db.sh
+++ ohp-db.sh
@@ -29,8 +29,8 @@ sleep 3;clear
 rm /usr/bin/ohp-dropbear
 cat> /usr/bin/ohp-dropbear << END
 #!/bin/bash
-screen -dmS dropbear ohpserver -port 50001 -proxy $MYIP:80443 -tunnel $MYIP:143
-screen -dmS openvpn ohpserver -port 50000 -proxy $MYIP:80443 -tunnel $MYIP:1194
+screen -dmS dropbear ohpserver -port 8181 -proxy $MYIP:3128 -tunnel $MYIP:143
+screen -dmS openvpn ohpserver -port 8282 -proxy $MYIP:3128 -tunnel $MYIP:22
 #screen -dmS vless ohpserver -port 8058 -proxy $MYIP:80443 -tunnel $MYIP:880
 END
 chmod +x /usr/bin/ohp-dropbear
@@ -39,8 +39,8 @@ echo -e "adding ohp-dropbear for ohpserver"
 sleep 3;clear
 cat> /usr/bin/ohp-dropbear << END
 #!/bin/bash
-screen -dmS dropbear ohpserver -port 50001 -proxy $MYIP:80443 -tunnel $MYIP:143
-screen -dmS openvpn ohpserver -port 50000 -proxy $MYIP:80443 -tunnel $MYIP:1194
+screen -dmS dropbear ohpserver -port 8181 -proxy $MYIP:3128 -tunnel $MYIP:143
+screen -dmS openvpn ohpserver -port 8282 -proxy $MYIP:3128 -tunnel $MYIP:22
 #screen -dmS vless ohpserver -port 8058 -proxy $MYIP:80443 -tunnel $MYIP:880
 END
 chmod +x /usr/bin/ohp-dropbear
@@ -65,23 +65,3 @@ END
 systemctl daemon-reload
 service ohp-dropbear start
 systemctl enable ohp-dropbear
-fi
-
-if [[ -e /root/log-ohp.txt  ]]; then
-rm /root/log-ohp.txt
-echo -e "Installation has been completed!!"
-echo ""
-echo ""
-echo "DROPBEAR : 8170" | tee -a log-ohp.txt
-echo "Openvpn Non : 50000" | tee -a log-ohp.txt
-echo ""
-echo ""
-read -n 1 -r -s -p $'Press any key to reboot...\n';reboot
-else
-echo -e "Installation has been completed!!"
-echo ""
-echo ""
-echo "DROPBEAR : 50001" | tee -a log-ohp.txt
-echo "OPENVPN : 50000" | tee -a log-ohp.txt
-echo ""
-fi
